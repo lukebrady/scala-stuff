f947fe861523cef1eb25bebb9b92f721ae776d28
Luke Brady <lbrad23105@gmail.com>
2018-12-26 21:33:45 -0500

    starting to work on inventory list in scala.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

---
+++ data/list.scala
@@ -0,0 +1,20 @@
+class ItemNode {
+    var next : ItemNode = null
+    var itemID: Int = null
+    var itemName: String = null
+    var weapon : Boolean = false
+    var food : Boolean = false
+    var potion : Boolean = false
+}
+
+object InventoryList {
+    // Print our current item to the screen.
+    def getCurrentItem = {
+
+    }
+}
+
+
+object Main extends App {
+
+}
